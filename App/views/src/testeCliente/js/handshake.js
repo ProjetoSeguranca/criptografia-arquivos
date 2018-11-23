09f482d1645f017558b6c0391dcc581ae7559dba
Vanderley Sousa <vanderley_1998@hotmail.com.br>
2018-11-23 15:02:32 -0200

    teste compartilhamento

--- App/views/src/testeCliente/js/handshake.js
+++ App/views/src/testeCliente/js/handshake.js
@@ -68,8 +68,8 @@ function CryptoJSAESEncryptSC(data) {
 
     var key = CryptoJS.PBKDF2(window.clientkey, salt, { hasher: CryptoJS.algo.SHA512, keySize: 64 / 8, iterations: 999 });
 
-    var nomeDestinatarioEncrypted = CryptoJS.AES.encrypt(data.login, key, { iv: iv });
-    var idArquivoEncrypted = CryptoJS.AES.encrypt(data.fileName, key, { iv: iv });
+    var nomeDestinatarioEncrypted = CryptoJS.AES.encrypt(data.nomeDestinatario, key, { iv: iv });
+    var idArquivoEncrypted = CryptoJS.AES.encrypt(data.idArquivo, key, { iv: iv });
 
     const nomeDestinatario = CryptoJS.enc.Base64.stringify(nomeDestinatarioEncrypted.ciphertext)
     const idArquivo = CryptoJS.enc.Base64.stringify(idArquivoEncrypted.ciphertext)
@@ -79,7 +79,7 @@ function CryptoJSAESEncryptSC(data) {
         idArquivo,
         salt: CryptoJS.enc.Hex.stringify(salt),
         iv: CryptoJS.enc.Hex.stringify(iv),
-        hash: CryptoJS.SHA256(JSON.stringify({nomeDestinatario, idArquivo})).toString()
+        hash: CryptoJS.SHA256(JSON.stringify({ nomeDestinatario, idArquivo })).toString()
     }
 }
 
